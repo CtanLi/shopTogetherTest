426020a207d24b14927456d5cc36167faa49eff7
Stanley <stanley@loyaltyworx.com>
2019-08-03 09:31:28 -0400

    New Project Set Up.

---
+++ shopTogether/shopTogether/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shopTogether
+//
+//  Created by CtanLI on 2019-08-03.
+//  Copyright © 2019 shopTogether. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
